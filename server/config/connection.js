01042a3002832e6bd8b0b266f66af2b82eee6382
ejasato <eric.j.asato@gmail.com>
2023-02-28 21:30:39 -0700

    mongodb database implemented and apollo framework

---
+++ server/config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/roadsage', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
