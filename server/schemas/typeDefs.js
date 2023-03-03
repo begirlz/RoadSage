46f270d785597495eb122989a5a09e594a1da337
ejasato <eric.j.asato@gmail.com>
2023-03-02 20:02:57 -0700

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,6 +21,8 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    saveTrip()
+    removeTrip()
   }
 `;
 
