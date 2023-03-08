4b3d9a108372ea9a8939ae43dbb99d9b15e2f552
begirlz <sw.begirlz@gmail.com>
2023-03-07 17:50:12 -0700

    update homepage with css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Home = () => {
-    return (
-      <div className="homepage">
-        {/* <CategoryMenu />
-        <ProductList />
-        <Cart /> */}
-      </div>
-    );
-  };
+  return (
+    <div className="homepage" id="big-box">
+        <div id="small-box" >
+          <h1 className="text-muted">The trip planner that gives you <span className='peace'>PEACE</span> instead of <span className="roadrage">ROAD RAGE</span></h1>
+        </div>
+    </div>
+  );
+};
 
 export default Home;
