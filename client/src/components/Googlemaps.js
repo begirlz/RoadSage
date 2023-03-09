139627a53d7a296cb37e91cdd0af0c2a1c494e74
Annemarie Sheridan <atsheridan38@gmail.com>
2023-03-08 17:13:59 -0800

    to get pull, nothing added

--- client/src/components/Googlemaps.js
+++ client/src/components/Googlemaps.js
@@ -115,4 +115,38 @@ if(!isLoaded){
   )
 }
 
+// const request = {
+//     query: "Museum of Contemporary Art Australia",
+//     fields: ["name", "geometry"],
+//   };
+
+//   service = new google.maps.places.PlacesService(map);
+//   service.findPlaceFromQuery(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+//       for (let i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
+
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
+// }
+
+// function createMarker(place) {
+//   if (!place.geometry || !place.geometry.location) return;
+
+//   const marker = new google.maps.Marker({
+//     map,
+//     position: place.geometry.location,
+//   });
+
+//   google.maps.event.addListener(marker, "click", () => {
+//     infowindow.setContent(place.name || "");
+//     infowindow.open(map);
+//   });
+// }
+
+// window.initMap = initMap;
+
+
 export default React.memo(MyComponent)
