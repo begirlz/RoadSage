c341a078c49149411df83f93f3354d5c1cfefde4
begirlz <sw.begirlz@gmail.com>
2023-03-09 04:43:19 -0700

    working resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,48 +39,18 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-
-
-    // addUser: async (parent, { username, email, password }) => {
-    //   const user = await User.create({ username, email, password });
-    //   const token = signToken(user);
-    //   return { token, user };
-    // },
-    // login: async (parent, { email, password }) => {
-    //   const user = await User.findOne({ email });
-
-    //   if (!user) {
-    //     throw new AuthenticationError('No user found with this email address');
-    //   }
-
-    //   const correctPw = await user.isCorrectPassword(password);
-
-    //   if (!correctPw) {
-    //     throw new AuthenticationError('Incorrect credentials');
-    //   }
-
-    //   const token = signToken(user);
-
-    //   return { token, user };
-    // },
-
-    //merge conflictc
-    // saveTrip: async (parent, { trip }, context) => {
-    //   console.log('mutation');
-    saveTrip: async (parent, { origin, destination }, context) => {
-      console.log('mutation1');
-      console.log(typeof origin);
-      console.log(origin);
+    saveTrip: async (parent, { trip }, context) => {
+      console.log('mutation');
       if (context.user) {
         console.log('mutation');
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { savedTrips: { origin: origin, destination: destination } } },
+          { $addToSet: { savedTrips: trip } },
           { new: true, runValidators: true }
         );
-        return updatedUser;
-      }
-      throw new AuthenticationError("Please log in");
+      return updatedUser;
+    }
+    throw new AuthenticationError("Please log in");
     },
     // removeTrip: async (parent, { tripId }, context) => {
     //   const updatedUser = await User.findOneAndUpdate(
